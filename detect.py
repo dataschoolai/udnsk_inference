957f52dc427e7ee9aa54256500df1b62ed36f2a3
ZARIF ubuntu <uzbzarif@gmail.com>
2022-02-09 17:29:07 +0900

    predict function

--- detect.py
+++ detect.py
@@ -40,45 +40,56 @@ path_weights = Path.cwd()/ 'yolo_model/Exposure_100/weights/best.pt'
 print("Path of weights: ", path_weights)
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_weights)
 
-#Input directory
-input_dir = Path.cwd()/'data/104'
-OUTPUT_DIR = make_output_dir('output')
-# input_dir = '/media/buntuml/DATASET/DAMAGEAI/REPORT/spalling/DATASET_JSON/images_list'
-#Create a list of all images_list in the directory
-images_list = [x for x in Path(input_dir).glob('*.jpg')]
-print(images_list[:10])
-#loop through all images_list
-for path in tqdm(images_list):
-    #Open image:PIL
-    image = Image.open(path)
 
-    # image = cv2.imread(str(path))
-    #Get prediction from model
-    prediction = model(image)
-    preds = []
-    #Get bounding boxes
-    if prediction.pred[0].shape[0]:
-        # list of tensors pred[0] = (xyxy, conf, cls)
-        for pred in prediction.pred[0]:
-            #Get bounding box
-            x1, y1, x2, y2 = pred[:4].tolist()
-            #Get confidence
-            conf = pred[4]
-            #Get class
-            cls = int(pred[5])
-            #Get class name
-            # cls_name = classes[cls]
-            #Append to list
-            preds.append([x1, y1, x2, y2, float(conf)])
+    
+#Define the function to predict image
+def predict(input_dir:Path,output_dir:Path):
+    """
+    Predict images in input directory
+    Parameters:
+    input_dir(Path): path to input directory
+    output_dir(Path): path to output directory
+    Returns:
+    None
+    """
+    #Create output directory if it does not exist
+    output_dir = make_output_dir(output_dir)
+    #Create a list of all images_list in the directory
+    images_list = [x for x in input_dir.glob('*.jpg')]
+    #loop through all images_list
+    for path in tqdm(images_list):
+        #Open image:PIL
+        image = Image.open(path)
+        # image = cv2.imread(str(path))
+        #Get prediction from model
+        prediction = model(image)
+        preds = []
+        #Get bounding boxes
+        if prediction.pred[0].shape[0]:
+            # list of tensors pred[0] = (xyxy, conf, cls)
+            for pred in prediction.pred[0]:
+                #Get bounding box
+                x1, y1, x2, y2 = pred[:4].tolist()
+                #Get confidence
+                conf = pred[4]
+                #Get class
+                cls = int(pred[5])
+                #Get class name
+                # cls_name = classes[cls]
+                #Append to list
+                preds.append([x1, y1, x2, y2, float(conf)])
         
         image_name = path.stem
         pred_label = prediction.names[0]
         #Save prediction to file in JSON format
-        output_file = OUTPUT_DIR / Path(image_name).with_suffix(suffix='.json') 
+        output_file = output_dir / Path(image_name).with_suffix(suffix='.json') 
         with open(output_file, 'w') as f:
             json.dump({'label':pred_label,'preds':preds}, f)
-        
 
-    break
-        #Save prediction to file
-        # prediction.save('output')
+
+#Input directory
+INPUT_DIR = Path.cwd()/'data/104'
+OUTPUT_DIR = make_output_dir('output')
+
+#Call the function to predict images
+predict(INPUT_DIR,OUTPUT_DIR)
