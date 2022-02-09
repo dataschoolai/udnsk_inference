caff9e7405b5071ee334b93fa029238c8597bcf2
ZARIF ubuntu <uzbzarif@gmail.com>
2022-02-09 17:19:41 +0900

    create a folder

--- detect.py
+++ detect.py
@@ -5,9 +5,30 @@ from PIL import Image
 import numpy as np
 #Import libarary for file and directory handling
 from pathlib import Path
+import shutil
 #Import library for machine learning
 import torch
 from tqdm import tqdm
+import json
+
+
+#Define the function to create output directory
+def make_output_dir(output_dir:str):
+    """
+    Create output directory if it does not exist
+    Parameters:
+    output_dir(str): path to output directory
+    Returns:
+    path(Path): path to output directory
+    """
+    path = Path(output_dir)
+    if path.exists():
+        shutil.rmtree(path)
+
+    path.mkdir(parents=True)
+    return path
+    
+
 #Get device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #print device
@@ -21,7 +42,7 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_weights)
 
 #Input directory
 input_dir = Path.cwd()/'data/104'
-
+OUTPUT_DIR = make_output_dir('output')
 # input_dir = '/media/buntuml/DATASET/DAMAGEAI/REPORT/spalling/DATASET_JSON/images_list'
 #Create a list of all images_list in the directory
 images_list = [x for x in Path(input_dir).glob('*.jpg')]
@@ -34,8 +55,30 @@ for path in tqdm(images_list):
     # image = cv2.imread(str(path))
     #Get prediction from model
     prediction = model(image)
+    preds = []
     #Get bounding boxes
     if prediction.pred[0].shape[0]:
-        prediction.files = [path.name]
+        # list of tensors pred[0] = (xyxy, conf, cls)
+        for pred in prediction.pred[0]:
+            #Get bounding box
+            x1, y1, x2, y2 = pred[:4].tolist()
+            #Get confidence
+            conf = pred[4]
+            #Get class
+            cls = int(pred[5])
+            #Get class name
+            # cls_name = classes[cls]
+            #Append to list
+            preds.append([x1, y1, x2, y2, float(conf)])
+        
+        image_name = path.stem
+        pred_label = prediction.names[0]
+        #Save prediction to file in JSON format
+        output_file = OUTPUT_DIR / Path(image_name).with_suffix(suffix='.json') 
+        with open(output_file, 'w') as f:
+            json.dump({'label':pred_label,'preds':preds}, f)
+        
+
+    break
         #Save prediction to file
-        prediction.save('output')
+        # prediction.save('output')
