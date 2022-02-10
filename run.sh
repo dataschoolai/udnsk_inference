60c87506189ca2789dfab8e997af6164beb15ccf
ZARIF ubuntu <uzbzarif@gmail.com>
2022-02-10 15:33:52 +0900

    run detect

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker run --rm --ipc=host --gpus all -it \
 -v $(pwd)/workspace:/usr/src/app/workspace \
-damage_detect bash
+damage_detect bash -c 'bash $workspace/detect.sh'
--- workspace/detect.sh
+++ workspace/detect.sh
@@ -1,4 +1,4 @@
-input_dir='data/104'
-output_dir='output/104'
-model_dir='yolo_model/Exposure_100/weights/best.pt'
+input_dir=$workspace/'data/104'
+output_dir=$workspace/'output/104'
+model_dir=$workspace/'yolo_model/Exposure_100/weights/best.pt'
 python detect.py --input_dir $input_dir --output_dir $output_dir --model_dir $model_dir
